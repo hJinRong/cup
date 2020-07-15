8b73fe41247b238cb3d78247411269328da40589
rong <rong161514@gmail.com>
2020-07-15 11:36:52 +0800

    use regex to filter the useful stdout

--- src/app/home/doctype/doctype.component.ts
+++ src/app/home/doctype/doctype.component.ts
@@ -39,14 +39,26 @@ export class DoctypeComponent implements OnInit {
   analyzeNow(): void {
     const pythonScript = this.cf.getFirstDir();
     const pythonProg = this.cf.getSecondDir();
+    // 为了避免没有设置路径就运行，结果导致找不到路径
+    this.cf.writeConfInRuntime();
     exec(
       `${pythonScript}/python.exe ${pythonProg}/my_deepsort.py --VIDEO_PATH ${this.dir} --weights ${pythonProg}/best4.pt --yolov3_cfg ${pythonProg}/yolov3-tiny-1cls-se.cfg --config_deepsort ${pythonProg}/configs/deep_sort.yaml`,
+
+      /**
+       * @param err 为了避免意外退出，不抛出错误
+       * @param stdout 可能不是一个单纯的JSON字符串，可能会导致 @function JSON.parse(stdout) 报错,
+       * 调用 @function JSON.parse(stdout) 前需要先正则匹配
+       */
       (err, stdout, stderr) => {
-        if (err) {
-          throw err;
-        }
-        console.log(JSON.parse(stdout));
-        this.fms.pushMessage(this.id, JSON.parse(stdout));
+        // if (err) {
+        //   throw err;
+        // }
+        let stdout1 = stdout.match(
+          /\{"data":(\s)*\[({"totalTime":(\s)*"[0-9]+\.[0-9]+",(\s)*"fps":(\s)*"[0-9]+\.[0-9]+"}(,)*(\s)*)*\](,)(\s)*"avg":(\s)*"[0-9]+\.[0-9]+"\}/g
+        )[0];
+        // console.log(stdout1);
+        // console.log(JSON.parse(stdout1));
+        this.fms.pushMessage(this.id, JSON.parse(stdout1));
       }
     );
   }
