16b4b41eb98e488f553659497b4694070784d38b
rong <rong161514@gmail.com>
2020-07-15 18:16:28 +0800

    set last frame dir

--- src/app/home/analyres/analyres.component.ts
+++ src/app/home/analyres/analyres.component.ts
@@ -1,23 +1,45 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges } from "@angular/core";
 import { FilemanageService } from "../../service/filemanage.service";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { FileItem } from "../doctype/fileitem";
 import { switchMap } from "rxjs/operators";
+import { DirectoryconfService } from "../../service/directoryconf.service";
 
 @Component({
   selector: "app-analyres",
   templateUrl: "./analyres.component.html",
   styleUrls: ["./analyres.component.css"],
 })
-export class AnalyresComponent implements OnInit {
+export class AnalyresComponent implements OnInit, OnChanges {
   fileitem: Observable<FileItem>;
+  filename: string;
 
-  constructor(private fms: FilemanageService, private route: ActivatedRoute) {}
+  public set lastFrameDir(v: string) {
+    this.lastFrameDir = v;
+  }
+
+  public get lastFrameDir(): string {
+    const dir = this.dc.getSecondDir();
+    this.fileitem.subscribe((x) => (this.filename = x.name));
+    return `file:///${dir}\\lastframe\\${this.filename}.jpg`;
+  }
+
+  constructor(
+    private fms: FilemanageService,
+    private dc: DirectoryconfService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.fileitem = this.route.paramMap.pipe(
       switchMap((params) => this.fms.getFile(params.get("id")))
     );
+  }
+
+  ngOnChanges(): void {
+    const dir = this.dc.getSecondDir();
+    this.fileitem.subscribe((x) => (this.filename = x.name));
+    this.lastFrameDir = `file:///${dir}\\lastframe\\${this.filename}.jpg`;
   }
 }
